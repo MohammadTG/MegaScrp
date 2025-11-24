4fd0995ed1dbf99393dce58704b4f2fd3eeacec6
MrMohammad <mhmdtahrghlamy54@gmail.com>
2025-11-24 11:50:03 +0330

    Add Some Futures

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MegaScrp",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="MohammadTaherGH",
+    description="توضیح کوتاه درباره ماژول",
+    url="https://github.com/MohammadTG/MegaScrp",
+    python_requires='>=3.8',
+)
